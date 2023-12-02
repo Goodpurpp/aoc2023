a0905a8e915a616e664b4fc15177b204036966e9
shegolev.a <shegolev.a@skbkontur.ru>
2023-12-03 00:26:18 +0500

    create class for task 3

---
+++ src/AOC/Tasks/Task3/AdventOfCodeTask3.java
@@ -0,0 +1,24 @@
+package AOC.Tasks.Task3;
+
+import AOC.AdventOfCodeTask;
+import AOC.Solving;
+
+import java.io.FileNotFoundException;
+
+public class AdventOfCodeTask3 extends AdventOfCodeTask implements Solving {
+
+
+    public AdventOfCodeTask3() throws FileNotFoundException {
+        super(3, "in.txt");
+    }
+
+    @Override
+    public void solveFirstPart() {
+
+    }
+
+    @Override
+    public void solveSecondPart() {
+
+    }
+}
