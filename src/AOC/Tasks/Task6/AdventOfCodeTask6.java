01a5d7f8d9309a446a7ec2930dfc82accf14c288
shegolev.a <shegolev.a@skbkontur.ru>
2023-12-06 14:17:37 +0500

    solve 6 day

--- src/AOC/Tasks/Task6/AdventOfCodeTask6.java
+++ src/AOC/Tasks/Task6/AdventOfCodeTask6.java
@@ -15,31 +15,46 @@ public class AdventOfCodeTask6 extends AdventOfCodeTask {
 
     @Override
     public void solveFirstPart() {
+        var start = System.currentTimeMillis();
         long ans = 1;
         List<String> input = getInputStringsTask();
+
         long[] time = Arrays.stream(input.get(0).split(" ")).skip(1)
                 .filter(l -> !l.isBlank())
                 .mapToLong(Long::parseLong).toArray();
+
         long[] dist = Arrays.stream(input.get(1).split(" ")).skip(1)
                 .filter(l -> !l.isBlank())
                 .mapToLong(Long::parseLong).toArray();
-        for(int i = 0; i < time.length;i++) {
+
+        for (int i = 0; i < time.length; i++) {
             final int q = i;
-            ans *= LongStream.range(1,time[i]).filter(sp-> dist[q] < (sp * (time[q] - sp))).count();
+            ans *= LongStream.range(1, time[i]).filter(sp -> dist[q] < (sp * (time[q] - sp))).count();
         }
+
         System.out.println(ans);
+
+        var end = System.currentTimeMillis();
+        System.out.printf("Time spent: %d millisecond\n", (end - start));
     }
 
     @Override
     public void solveSecondPart() {
-        long ans = 1;
+        var start = System.currentTimeMillis();
+        long ans;
         List<String> input = getInputStringsTask();
-        long time = Arrays.stream(input.get(0).split(":")[1].split(" ")).filter(l -> !l.isBlank())
-                .mapToLong(Long::parseLong)
-                .reduce((x,y) -> ((x * 100) + y)).getAsLong();
-        long dist = Long.parseLong(Arrays.stream(input.get(1).split(":")[1].split(" ")).filter(l -> !l.isBlank())
-                .reduce((x,y) -> (x + y)).get());
-        ans *= LongStream.range(1,time).filter(sp-> dist < (sp * (time - sp))).count();
+
+        long time = Long.parseLong(Arrays.stream(input.get(0).split(":")[1].split(" "))
+                .filter(l -> !l.isBlank())
+                .reduce(String::concat).get());
+
+        long dist = Long.parseLong(Arrays.stream(input.get(1).split(":")[1].split(" "))
+                .filter(l -> !l.isBlank())
+                .reduce(String::concat).get());
+
+        ans = LongStream.range(1, time).filter(sp -> dist < (sp * (time - sp))).count();
         System.out.println(ans);
+        var end = System.currentTimeMillis();
+        System.out.printf("Time spent: %d millisecond\n", (end - start));
     }
 }
